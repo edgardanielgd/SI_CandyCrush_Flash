b4f71af669e2c842c3a04dc6e6617fdcf2daa208
edgardanielgd <edgardanielgd123@gmail.com>
2023-08-31 19:09:11 -0500

    Returned templates to old images

--- testImageCrop.cpp
+++ testImageCrop.cpp
@@ -7,7 +7,11 @@ using namespace std;
 int main()
 {
     vector<cv::Mat> matTemplates = getTemplates();
-    cv::Mat src = cv::imread("output/cell_7_6.png", cv::IMREAD_UNCHANGED);
+    cv::Mat src = cv::imread("output/cell_0_2.png", cv::IMREAD_UNCHANGED);
+
+    // cv::cvtColor(src, src, cv::COLOR_BGR2GRAY);
+
+    cv::imwrite("output_test_src.png", src);
 
     int classCount = 1;
 
@@ -15,20 +19,21 @@ int main()
     {
         cv::Mat toCompare;
         cv::resize(tem, toCompare, cv::Size(src.cols, src.rows), cv::INTER_LINEAR);
+        // cv::cvtColor(toCompare, toCompare, cv::COLOR_BGR2GRAY);
+        // cv::imwrite("output_test_tmp.png", toCompare);
 
-        // cout << "Source: " << src.rows << " " << src.cols << endl;
-        // cout << "ToCompare: " << toCompare.rows << " " << toCompare.cols << endl;
+        cv::Mat result;
+        // cv::matchTemplate(src, toCompare, result, cv::TM_CCORR_NORMED);
+
+        // Get maximum probability
+        // double minLoc, maxLoc, minVal, maxVal;
+
+        // cv::minMaxLoc(result, &minVal, &maxVal);
 
         cv::Scalar ssim = cv::PSNR(src, toCompare);
 
+        // double similarity = cv::matchShapes(src, toCompare, cv::CONTOURS_MATCH_I1, 0);
         cout << "SSIM: " << ssim[0] << " " << classCount << endl;
-        // cv::matchTemplate(
-        //     src, tem, res, cv::TM_CCOEFF_NORMED);
-
-        // cv::threshold(res, res, 0.0, 1, cv::THRESH_BINARY);
-
-        // cv::imwrite("output/res_" + to_string(classCount) + ".png", res);
-
         classCount++;
     }
 }
